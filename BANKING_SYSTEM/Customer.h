59dde4eb711b44f11398bd29dbbcbb8a3157cf8f
HiraIshtiaq <hiraishtiaq0716@gmail.com>
2025-08-24 03:45:34 -0700

    Add project files.

---
+++ BANKING_SYSTEM/Customer.h
@@ -0,0 +1,22 @@
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+#include <string>
+using namespace std;
+
+class Customer 
+{
+private:
+
+    int customerId;
+    string name;
+    string address;
+
+public:
+    Customer(int id = 0, string n = "", string addr = "");
+    void display() const;
+    int getId() const;
+    string getName() const;
+};
+
+#endif
